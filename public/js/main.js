472a08db4b7bee014158aa54b6dc95c1257d54a1
RishkoSergey <risko_sergey@mail.ru>
2019-03-27 22:48:27 +0300

    index page

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,7 @@
-'user strict'
+'use strict'
 
-alert('Hello, world!');
+$.get( '/api/marks', ( data ) => {
+    data.forEach(mark => {
+        $('#container table tbody').append(`<tr><td>${mark.semester}</td><td>${mark.subject}</td><td>${mark.mark}</td></tr>`);     
+    });    
+});
