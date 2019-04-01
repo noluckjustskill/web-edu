292daec55274bf4e6d39701252c8a33bf53cbbe4
RishkoSergey <risko_sergey@mail.ru>
2019-04-01 21:10:49 +0300

    information controller, main page

--- node/controller/index.ts
+++ node/controller/index.ts
@@ -1,9 +1,7 @@
 import { app } from '../app';
-import Client from './client';
 import Marks from './marks';
 
 export default (url: string = ''): void => {
 
-  app.get(`${url}/client`, Client);
   app.get(`${url}/marks`, Marks);
 };
